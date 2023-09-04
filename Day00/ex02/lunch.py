729b3ebd4cb8cde319510657e8e2848daafc2fd2
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-09-04 18:12:09 +0200

    Updated volume, everything is working as expected

--- Day00/ex02/lunch.py
+++ Day00/ex02/lunch.py
@@ -2,9 +2,9 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 try:
-    data = pd.read_csv("/goinfre/kvebers/subject/customer/data_2022_dec.csv")
+    data = pd.read_csv("/goinfre/kvebers/subject/customer/data_2022_oct.csv")
     engine = create_engine("postgresql://kvebers:mysecretpassword@localhost:5432/piscineds")
-    data.to_sql("ex02", engine, if_exists='replace', index=False)
+    data.to_sql("data_2022_oct", engine, if_exists='replace', index=False)
     engine.dispose()
 except Exception as e:
     print(f"An error occurred: {e}")
