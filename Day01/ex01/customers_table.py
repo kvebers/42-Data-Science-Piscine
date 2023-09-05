c52f86d78e438dcde06bfb3987248ef732ecd20e
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-09-05 14:16:48 +0200

    IT WORKS

--- Day01/ex01/customers_table.py
+++ Day01/ex01/customers_table.py
@@ -1,11 +1,37 @@
+import os
 import pandas as pd
 from sqlalchemy import create_engine
 import sqlalchemy
 
 
-def load(path, tableName):
+def readData(path):
+    data = pd.read_csv(path)
+    return (data)
+
+
+def extractTableNames(item):
+    return [name[0:-4] for name in item]
+
+
+def checkCSV(item):
+    if (len(item) < 4):
+        return False
+    if (item[-4:] != ".csv"):
+        return False
+    return True
+
+
+def main():
+    path = "/goinfre/kvebers/subject/customer/"
+    tableName = "customers"
     try:
-        data = pd.read_csv(path)
+        Files = os.listdir(path)
+        TablesToAdd = [item for item in Files if checkCSV(item) is True]
+        data = pd.DataFrame()
+        for item in TablesToAdd:
+            full_path = os.path.join(path, item)
+            data = pd.concat([data, readData(full_path)], ignore_index=True)
+
         data_types = {
             "event_time": sqlalchemy.DateTime(),
             "event_type":  sqlalchemy.types.String(length=255),
@@ -14,7 +40,6 @@ def load(path, tableName):
             "user_id": sqlalchemy.types.BigInteger(),
             "user_session": sqlalchemy.types.Uuid()
         }
-
         engine = create_engine("postgresql://kvebers:mysecretpassword@localhost:5432/piscineds")
         data.to_sql(tableName, engine, index=False, dtype=data_types)
         engine.dispose()
@@ -23,4 +48,4 @@ def load(path, tableName):
 
 
 if __name__ == "__main__":
-    load("/goinfre/kvebers/subject/customer/data_2022_oct.csv", "data_2022_oct")
+    exit(main())
