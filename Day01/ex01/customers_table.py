d9884cf45f7354832255a62d3c8b80cead48aecc
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-09-11 11:23:54 +0200

    Slow, but it works

--- Day01/ex01/customers_table.py
+++ Day01/ex01/customers_table.py
@@ -3,37 +3,18 @@ import pandas as pd
 from sqlalchemy import create_engine, inspect
 from table import load
 
-def readData(path):
-    data = pd.read_csv(path)
-    return (data)
-
-
-def extractTableNames(item):
-    return [name[0:-4] for name in item]
-
-
-def checkCSV(item):
-    if (len(item) < 4):
-        return False
-    if (item[-4:] != ".csv"):
-        return False
-    return True
-
-
 def main():
     path = "/goinfre/kvebers/subject/customer/"
     tableName = "customers"
     try:
+        print("I did something")
         Files = os.listdir(path)
-        TablesToAdd = [item for item in Files if checkCSV(item) is True]
-        engine = create_engine("postgresql://kvebers:mysecretpassword@localhost:5432/piscineds")
-        inspector = inspect(engine)
-        if inspector.has_table(tableName):
-            engine.execute(f"DROP TABLE IF EXISTS {tableName}")
-        engine.dispose()
+        TablesToAdd = [item for item in Files]
+        print(TablesToAdd)
         for item in TablesToAdd:
             full_path = os.path.join(path, item)
             load(full_path, tableName)
+            print(full_path)
     except Exception as e:
         print(f"An error occurred: {e}")
 
