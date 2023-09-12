334f19d0d6c7ae7efac41e8ab44ce899ac8a0340
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-09-12 14:56:52 +0200

    Just Day 01 done

---
+++ Day02/ex00/setup/script_to_execute.sql
@@ -0,0 +1,104 @@
+CREATE TABLE IF NOT EXISTS data_2022_oct (
+    event_time TIMESTAMP,
+    event_type VARCHAR(255),
+    product_id INT,
+    price FLOAT DEFAULT 0,
+    user_id BIGINT DEFAULT -1,
+    user_session UUID DEFAULT '00000000-0000-0000-0000-000000000000'
+);
+
+COPY data_2022_oct FROM '/tmp/data_2022_oct.csv' CSV HEADER;
+
+CREATE TABLE IF NOT EXISTS data_2022_nov (
+    event_time TIMESTAMP,
+    event_type VARCHAR(255),
+    product_id INT,
+    price FLOAT DEFAULT 0,
+    user_id BIGINT DEFAULT -1,
+    user_session UUID DEFAULT '00000000-0000-0000-0000-000000000000'
+);
+
+COPY data_2022_nov FROM '/tmp/data_2022_nov.csv' CSV HEADER;
+
+CREATE TABLE IF NOT EXISTS data_2022_dec (
+    event_time TIMESTAMP,
+    event_type VARCHAR(255),
+    product_id INT,
+    price FLOAT DEFAULT 0,
+    user_id BIGINT DEFAULT -1,
+    user_session UUID DEFAULT '00000000-0000-0000-0000-000000000000'
+);
+
+COPY data_2022_dec FROM '/tmp/data_2022_dec.csv' CSV HEADER;
+
+
+
+CREATE TABLE IF NOT EXISTS data_2023_jan (
+    event_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    event_type VARCHAR(255) DEFAULT 'unknown_event',
+    product_id INT DEFAULT -1,
+    price FLOAT DEFAULT -1.0,
+    user_id BIGINT DEFAULT -1,
+    user_session UUID DEFAULT '00000000-0000-0000-0000-000000000000'
+);
+
+COPY data_2023_jan FROM '/tmp/data_2023_jan.csv' CSV HEADER;
+
+CREATE TABLE IF NOT EXISTS data_2023_feb (
+    event_time TIMESTAMP,
+    event_type VARCHAR(255),
+    product_id INT,
+    price FLOAT DEFAULT 0,
+    user_id BIGINT DEFAULT -1,
+    user_session UUID DEFAULT '00000000-0000-0000-0000-000000000000'
+);
+
+COPY data_2023_feb FROM '/tmp/data_2023_feb.csv' CSV HEADER;
+
+CREATE TABLE IF NOT EXISTS items (
+    product_id INT,
+    category_id BIGINT,
+    category_code VARCHAR(255),
+    brand VARCHAR(255)
+);
+
+COPY items FROM '/tmp/item.csv' CSV HEADER;
+
+
+CREATE TABLE IF NOT EXISTS customers (
+    event_time TIMESTAMP,
+    event_type VARCHAR(255),
+    product_id INT,
+    price FLOAT,
+    user_id BIGINT,
+    user_session UUID
+);
+INSERT INTO customers (event_time, event_type, product_id, price, user_id, user_session)
+(
+    SELECT event_time, event_type, product_id, price, user_id, user_session FROM data_2022_oct
+    UNION ALL
+    SELECT event_time, event_type, product_id, price, user_id, user_session FROM data_2022_nov
+    UNION ALL
+    SELECT event_time, event_type, product_id, price, user_id, user_session FROM data_2022_dec
+    UNION ALL
+    SELECT event_time, event_type, product_id, price, user_id, user_session FROM data_2023_feb
+    UNION ALL
+    SELECT event_time, event_type, product_id, price, user_id, user_session FROM data_2023_jan
+);
+
+CREATE TEMPORARY TABLE temp AS SELECT DISTINCT * FROM customers;
+TRUNCATE customers;
+INSERT INTO customers SELECT * FROM temp;
+
+ALTER TABLE customers
+ADD COLUMN category_id BIGINT,
+ADD COLUMN category_code VARCHAR(255),
+ADD COLUMN brand VARCHAR(255);
+
+UPDATE customers c
+SET 
+    category_id = i.category_id,
+    category_code = i.category_code,
+    brand = i.brand
+FROM items i
+WHERE c.product_id = i.product_id;
