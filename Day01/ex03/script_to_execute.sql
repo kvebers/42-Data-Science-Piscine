d528c5a8349acddc315a325b7ff0fa834fc12a10
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-09-13 09:53:00 +0200

    Forgot to add this

--- Day01/ex03/script_to_execute.sql
+++ Day01/ex03/script_to_execute.sql
@@ -4,9 +4,9 @@ ADD COLUMN category_code VARCHAR(255),
 ADD COLUMN brand VARCHAR(255);
 
 UPDATE customers c
-SET
+SET 
     category_id = i.category_id,
     category_code = i.category_code,
     brand = i.brand
-FROM items_nodup i
+FROM items i
 WHERE c.product_id = i.product_id;
